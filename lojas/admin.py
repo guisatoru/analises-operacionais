dad0e599a91252345f64aebf6a625b2f56f8da21
Gui Satoru <guisatoru11@gmail.com>
2026-05-08 15:49:41 -0300

    Remove modelos EscopoLoja e ItemEscopo e aplica migration de drop

--- lojas/admin.py
+++ lojas/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
-from .models import Cargo, EscopoLoja, ItemEscopo, Loja, Salario
+from .models import Cargo, EscopoMensal, ItemEscopoMensal, Loja, Salario
+
 
 @admin.register(Loja)
 class LojaAdmin(admin.ModelAdmin):
     list_display = (
-        "ver_escopos",
+        "ver_escopos_mensais",
         "nome_referencia",
         "centro_de_custo",
         "cliente",
@@ -26,51 +27,67 @@ class LojaAdmin(admin.ModelAdmin):
     readonly_fields = ("created_at", "updated_at")
 
     fieldsets = (
-        ("Identificação", {
-            "fields": (
-                "nome_referencia",
-                "centro_de_custo",
-                "quadro",
-                "status",
-                "cliente",
-                "cnpj",
-                "codigo_loja",
-                "nome_metricas",
-            ),
-        }),
-        ("Nomes em outros sistemas", {
-            "fields": (
-                "nome_geovictoria",
-                "nome_gestao",
-                "nome_totvs",
-                "nome_financeiro",
-                "nome_findme",
-            ),
-        }),
-        ("Endereço", {
-            "fields": (
-                "cep",
-                "rua",
-                "bairro",
-                "municipio",
-                "uf",
-                "sub_regiao",
-            ),
-        }),
-        ("Controle", {
-            "fields": ("created_at", "updated_at"),
-        }),
+        (
+            "Identificação",
+            {
+                "fields": (
+                    "nome_referencia",
+                    "centro_de_custo",
+                    "quadro",
+                    "status",
+                    "cliente",
+                    "cnpj",
+                    "codigo_loja",
+                    "nome_metricas",
+                ),
+            },
+        ),
+        (
+            "Nomes em outros sistemas",
+            {
+                "fields": (
+                    "nome_geovictoria",
+                    "nome_gestao",
+                    "nome_totvs",
+                    "nome_financeiro",
+                    "nome_findme",
+                ),
+            },
+        ),
+        (
+            "Endereço",
+            {
+                "fields": (
+                    "cep",
+                    "rua",
+                    "bairro",
+                    "municipio",
+                    "uf",
+                    "sub_regiao",
+                ),
+            },
+        ),
+        (
+            "Controle",
+            {
+                "fields": ("created_at", "updated_at"),
+            },
+        ),
     )
 
     @admin.display(description="Escopos")
-    def ver_escopos(self, obj):
-        url = reverse("admin:lojas_escopoloja_changelist")
-        return format_html('<a href="{}?loja__id__exact={}">Ver escopos</a>', url, obj.id)
+    def ver_escopos_mensais(self, obj):
+        url = reverse("admin:lojas_escopomensal_changelist")
+        return format_html(
+            '<a href="{}?loja__id__exact={}">Ver escopos</a>', url, obj.id
+        )
+
 
 @admin.register(Cargo)
 class CargoAdmin(admin.ModelAdmin):
     search_fields = ("nome",)
     list_display = ("nome",)
+
 
 @admin.register(Salario)
 class SalarioAdmin(admin.ModelAdmin):
@@ -78,17 +95,16 @@ class SalarioAdmin(admin.ModelAdmin):
     list_filter = ("uf", "ano")
     search_fields = ("cargo__nome",)
 
-class ItemEscopoInline(admin.TabularInline):
-    model = ItemEscopo
+
+class ItemEscopoMensalInline(admin.TabularInline):
+    model = ItemEscopoMensal
     extra = 0
     autocomplete_fields = ("cargo",)
 
-@admin.register(EscopoLoja)
-class EscopoLojaAdmin(admin.ModelAdmin):
-    def status_fim(self, obj):
-        return obj.data_fim or "Vigente"
-    status_fim.short_description = "Data fim"
-    list_display = ("loja", "data_inicio", "status_fim")
-    list_filter = ("loja",)
+
+@admin.register(EscopoMensal)
+class EscopoMensalAdmin(admin.ModelAdmin):
+    list_display = ("loja", "ano", "mes")
+    list_filter = ("loja", "ano", "mes")
     autocomplete_fields = ("loja",)
-    inlines = [ItemEscopoInline]
+    inlines = [ItemEscopoMensalInline]
